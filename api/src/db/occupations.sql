2053086fca93dc0f2c7fb0f0dccc54e042175f7f
Fernando H. Filter <ff@interact.com.br>
2023-04-10 13:41:36 -0300

    Created Insert Schemas for Selects

---
+++ api/src/db/occupations.sql
@@ -0,0 +1,12 @@
+INSERT INTO consultas.occupations 
+   	(name)
+VALUES
+    ('Cardiologista'),
+    ('Clínico geral'),
+    ('Dermatologista'),
+    ('Geriatra'),
+    ('Ginecologista'),
+    ('Nutricionista'),
+    ('Ortopedista'),
+    ('Psicólogo'),
+    ('Psiquiatra');
