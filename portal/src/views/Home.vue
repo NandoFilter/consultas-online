d6dbb7d0bfae945a0084df149e3a1160149408b6
Fernando Halmenschlager Filter <fernando.filter@universo.univates.br>
2023-05-14 16:53:11 -0300

    Starting Doctors Cards

--- portal/src/views/Home.vue
+++ portal/src/views/Home.vue
@@ -2,6 +2,13 @@
   <div>
     <Navigation />
     <div class="main">
+      <h1>Converse com um profissional</h1>
+      <div>
+        <div class="card">
+          <v-icon icon="mdi-brain" />
+          <span>Psicólogo</span>
+        </div>  
+      </div>
     </div>
   </div>
 </template>
@@ -20,5 +27,26 @@ export default defineComponent({
 <style lang="scss" scoped>
 .main {
   margin-left: 75px;
+}
+
+.card {
+  width: 15vh;
+  height: 10vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  border-radius: 15px;
+  box-shadow: 0px 3px 5px gray;
+}
+
+.card:hover {
+  cursor: pointer;
+
+  color: $primary-color;
+
+  box-shadow: 0px 3px 5px $primary-color;
 }
 </style>
