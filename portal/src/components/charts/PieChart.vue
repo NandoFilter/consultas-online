bd4174617ba36f8c15f1423eca66088b1d8ffaa7
Fernando H. Filter <ff@interact.com.br>
2023-05-15 13:57:02 -0300

    Trying to get values from DB to Chart

--- portal/src/components/charts/PieChart.vue
+++ portal/src/components/charts/PieChart.vue
@@ -3,9 +3,12 @@
 </template>
   
 <script lang="ts">
+import { defineComponent } from 'vue'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'vue-chartjs'
-import ChartConfig from './chartConfig'
+import { Deficiency } from '../../models'
+import { DeficiencyService } from '@/services'
+import { toRaw } from 'vue'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
@@ -14,23 +17,57 @@ type Data = {
   datasets: any[]
 }
 
-export default {
+export default defineComponent ({
   name: 'App',
   components: {
     Pie
   },
+  async created() {
+    this.deficiencies = await DeficiencyService.getAll()
+  },
   data: () => ({
     data: {} as Data,
     options: {},
+    deficiencies: [] as Deficiency[]
   }),
   methods: {
-    getConfig() {
-      this.data = ChartConfig.getDeficiencies()
-      this.options = ChartConfig.getOptions()
+    async getDeficiencyNames() {
+      // let names: string[] = []
+      // const deficiencies: Deficiency[] = toRaw(await DeficiencyService.getAll())
+
+      // deficiencies.forEach((deficiency) => {
+      //   names.push(deficiency.name)
+      // })
+
+      return null
+    },
+    async getConfig() {
+      const test = ['Visual', 'Motora', 'Surdez', 'Autismo', 'Síndrome de Down']
+
+      this.data = {
+        labels: test,
+        datasets: [
+          {
+            backgroundColor: [
+              '#0EA7C7',
+              '#0FD1BE',
+              '#16B97A',
+              '#0FD150',
+              '#0EC717',
+            ],
+            data: [40, 20, 80, 10, 50],
+          },
+        ],
+      }
+
+      this.options = {
+        responsive: true,
+        maintainAspectRatio: false,
+      }
     }
   },
-  beforeMount() {
-    this.getConfig()
+  async beforeMount() {
+    await this.getConfig()
   }
-}
+})
 </script>
--- portal/src/components/charts/chartConfig.ts
+++ portal/src/components/charts/chartConfig.ts
@@ -1,14 +1,34 @@
+import { DeficiencyService } from '@/services'
+
 class ChartConfig {
-  static getDeficiencies() {
+  private static async getDeficienciesNames() {
+    const names: string[] = []
+
+    const deficiencies = await DeficiencyService.getAll()
+
+    deficiencies.forEach((dependency) => {
+      names.push(dependency.name)
+    })
+
+    return names
+  }
+
+  static async getDeficiencies() {
     return {
-      labels: ['Autismo', 'Audição', 'Visão', 'Motora', 'Síndrome de Down'],
+      labels: this.getDeficienciesNames(),
       datasets: [
         {
-          backgroundColor: ['#0EA7C7', '#0FD1BE', '#16B97A', '#0FD150', '#0EC717'],
-          data: [40, 20, 80, 10, 50]
-        }
-      ]
-    } 
+          backgroundColor: [
+            '#0EA7C7',
+            '#0FD1BE',
+            '#16B97A',
+            '#0FD150',
+            '#0EC717',
+          ],
+          data: [40, 20, 80, 10, 50],
+        },
+      ],
+    }
   }
 
   static getOptions() {
@@ -20,4 +40,3 @@ class ChartConfig {
 }
 
 export default ChartConfig
-  
