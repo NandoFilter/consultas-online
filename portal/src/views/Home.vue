70ef3647c1854e2e53edc072c81e72b94e09711f
Fernando Halmenschlager Filter <fernando.filter@universo.univates.br>
2023-05-17 20:34:57 -0300

    Update Home.vue

--- portal/src/views/Home.vue
+++ portal/src/views/Home.vue
@@ -3,10 +3,10 @@
     <Navigation />
     <div class="main">
       <h1>Converse com um profissional</h1>
-      <div>
-        <div class="card">
-          <v-icon icon="mdi-brain" />
-          <span>Psicólogo</span>
+      <div class="items">
+        <div class="card" v-for="item in items" :key="item.id">
+          <v-icon :icon="item.icon" />
+          <span>{{ item.title }}</span>
         </div>  
       </div>
     </div>
@@ -20,33 +20,62 @@ import { Navigation } from '@/components';
 export default defineComponent({
   components: {
     Navigation
-  }
+  },
+  data: () => ({
+    items: [
+      { id: 0, title: 'Psicólogo',     icon: 'mdi-brain' },
+      { id: 1, title: 'Nutricionista', icon: 'mdi-food-apple-outline' },
+      { id: 2, title: 'Cardiologista', icon: 'mdi-heart-outline' },
+      { id: 3, title: 'Dentista',      icon: 'mdi-tooth-outline' },
+    ]
+  })
 })
 </script>
 
 <style lang="scss" scoped>
 .main {
   margin-left: 75px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.items {
+  display: flex;
+  flex-direction: row;
 }
 
 .card {
-  width: 15vh;
-  height: 10vh;
+  width: 18vh;
+  height: 12vh;
+
+  margin: 10px;
+
+  border-radius: 15px;
+
+  color: #000;
+  font-size: 20px;
+  
+  box-shadow: 0px 1px 3px gray;
+
+  transition: box-shadow 0.5s ease;
+  transition: color 0.5s ease;
 
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-
-  border-radius: 15px;
-  box-shadow: 0px 3px 5px gray;
 }
 
 .card:hover {
   cursor: pointer;
 
+  transition: box-shadow 0.5s ease;
+  transition: color 0.5s ease;
+
   color: $primary-color;
 
-  box-shadow: 0px 3px 5px $primary-color;
+  box-shadow: 0px 1px 3px $primary-color;
 }
 </style>
